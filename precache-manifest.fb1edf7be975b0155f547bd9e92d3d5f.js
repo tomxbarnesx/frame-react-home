c56ad6fd4de6c12b0e30b655f0dbbfcd80002523
Tom Barnes <barnes.thomas.c@gmail.com>
2019-01-28 22:02:49 -0500

    Updates

---
+++ precache-manifest.fb1edf7be975b0155f547bd9e92d3d5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ade99bcd9ebfd0142fe7dff9b9daf47",
+    "url": "/frame-react-home/static/media/whiteLogo.8ade99bc.png"
+  },
+  {
+    "revision": "929fa87f83bf7ef582d4a319f85691e9",
+    "url": "/frame-react-home/static/media/frame-bg.929fa87f.jpg"
+  },
+  {
+    "revision": "4fe9c6050e99400a512d8e010541806b",
+    "url": "/frame-react-home/static/media/downArrow.4fe9c605.png"
+  },
+  {
+    "revision": "c1caec5508f19016b6ae",
+    "url": "/frame-react-home/static/js/runtime~main.c1caec55.js"
+  },
+  {
+    "revision": "bd09c1a7bcda3c1e367f",
+    "url": "/frame-react-home/static/js/main.bd09c1a7.chunk.js"
+  },
+  {
+    "revision": "ac9ae1edcdde807d9986",
+    "url": "/frame-react-home/static/js/1.ac9ae1ed.chunk.js"
+  },
+  {
+    "revision": "bd09c1a7bcda3c1e367f",
+    "url": "/frame-react-home/static/css/main.3802af28.chunk.css"
+  },
+  {
+    "revision": "8745ea1353eabdfefdb1231094fa882f",
+    "url": "/frame-react-home/index.html"
+  }
+];
