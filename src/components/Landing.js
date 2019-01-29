71383a088eaaf18d219ac4d5fb00bfbe86b2a029
Tom Barnes <barnes.thomas.c@gmail.com>
2019-01-29 10:09:34 -0500

    react build init

---
+++ src/components/Landing.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import whiteLogo from '../images/whiteLogo.png';
+import downArrow from '../images/downArrow.png';
+
+class Landing extends Component {
+    render(){
+        return (
+        <div>
+            <nav className="topNav">
+                <div className="navItem"><strong>ABOUT</strong></div>
+                <div className="navItem"><strong>CONTACT</strong></div>    
+            </nav>
+            <div className="logoContainer"> 
+                <img className="centerLogo" src={ whiteLogo } alt="Frame logo"/>
+                <div className="logoBreak"></div>
+                <div className="logoSub">
+                    <h2 className="logoH2">A NEWS MAGAZINE FOR THIS CENTURY</h2>
+                </div>
+            </div>
+            <div className="landingButtons">
+                <a><div className="signUpButton"><strong>SIGN UP FREE OUR INAUGURAL MONTH</strong></div></a>
+                <a><div className="signUpButton"><strong>BECOME A FOUNDING SUBSCRIBER</strong></div></a>
+            </div>
+            <div className="downPoint">
+                <img src={ downArrow } width="100%" alt="down scroll"/>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Landing;
