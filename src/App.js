51b0ae516e1f57fcfe3fd3f698cdb97378629693
Tom Barnes <barnes.thomas.c@gmail.com>
2019-02-01 16:46:16 -0500

    delta + scroll override implemented

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import featureData from './data/features';
 
 import Landing from './components/Landing';
 import Feature from './components/Feature';
+import ParticleWrapper from './components/ParticleWrapper'
 // import AltFeature from './components/AltFeature';
-import Particles from 'react-particles-js';
-
 
 class App extends Component {
   constructor() {
@@ -16,9 +15,13 @@ class App extends Component {
     this.state = {
       featureData: featureData,
       activeFeature: 0,
-      scroll: 0
+      scroll: 0,
+      delta: 0
     }
     this.handleScroll = this.handleScroll.bind(this)
+    this.disableScroll = this.disableScroll.bind(this)
+    this.MouseWheelHandler = this.MouseWheelHandler.bind(this)
+
     // this.handleChange = this.handleChange.bind(this)
   }
 
@@ -41,184 +44,91 @@ class App extends Component {
     }
   }
 
+  disableScroll(){
+    if (window.addEventListener) {
+      // IE9, Chrome, Safari, Opera
+      window.addEventListener("mousewheel", this.MouseWheelHandler, false);
+      // Firefox
+      window.addEventListener("DOMMouseScroll", this.MouseWheelHandler, false);
+    }
+    // IE 6/7/8
+    else window.attachEvent("onmousewheel", this.MouseWheelHandler);
+  }
+
+  MouseWheelHandler(e) {
+    var e = window.event || e; // old IE support
+    if (this.state.delta <= 2){
+      this.setState(prevState => {
+        return {
+          delta: prevState.delta + Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)))
+        }
+      })
+    }
+    console.log("DELTA", this.state.delta)
+    if (this.state)
+    this.setState(prevState => {
+      return {
+        activeFeature: prevState.activeFeature + 1 
+      }
+    })
+    e.preventDefault()
+    return false;
+  }
+
   handleScroll() {
     const scrollHeight = document.body.scrollHeight;
     const windowHeight = window.innerHeight
-    const top = this.top;
     const scrollTop = window.scrollY;
-    let scrollAmount = (scrollTop / (scrollHeight-windowHeight)) * 100 // get amount scrolled (in %)
+    let scrollDirection = undefined;
+    let scrollAmount = (scrollTop / (scrollHeight-windowHeight)) * 100; // get amount scrolled (in %)
+
+    if (scrollAmount > 32.5){
+      this.disableScroll();
+      // if (scrollAmount > this.state.scroll + 5 && this.state.activeFeature < 2) {
+      //   scrollDirection = "BIG down";
+      //   this.setState(prevState => {
+      //     return {
+      //       activeFeature: prevState.activeFeature + 1 
+      //     }
+      //   })
+      //   console.log(scrollDirection);
+      //   console.log(this.state.activeFeature)
+  
+      // } else if (scrollAmount < this.state.scroll - 5 && this.state.activeFeature > 0) {
+      //   scrollDirection = "BIG up";
+      //   this.setState(prevState => {
+      //     return {
+      //       activeFeature: prevState.activeFeature - 1 
+      //     }
+      //   })
+      //   console.log(scrollDirection);
+      //   console.log(this.state.activeFeature)
+      // }
+      // return false;
+    }
+
     this.setState(prevState => {
       return {
         scroll: scrollAmount
       }
     })
-    console.log('page top', scrollAmount);
 
-    if (scrollAmount < 25) {
-      this.setState(prevState => {
-        return {
-          activeFeature: 0
-        }
-      })
-    }
-    if (scrollAmount > 25) {
-      setTimeout(
-        function() {
-          this.setState(prevState => {
-            return {
-              activeFeature: 1
-            }
-          })
-        }
-        .bind(this), 
-        200
-      )
-    }
-    if (scrollAmount > 45) {
-      setTimeout(
-        function() {
-          this.setState(prevState => {
-            return {
-              activeFeature: 2
-            }
-          })
-        }
-        .bind(this), 
-        200
-      )
-    }
+    console.log('page top', this.state.scroll);
   }
-  // handleChange() {
-  //   if (this.state.scroll > 25) {
-  //     this.setState(prevState => {
-  //         return {
-  //             active: 1
-  //         }
-  //     })
-  //   }
-  //   console.log("FIRING!")
-  // }
 
   render() {
+    const features = this.state.featureData.map((feature) => {
+      return <Feature key={feature.id} info={feature} scroll={this.state.scroll} active={this.state.delta} />
+    })
+
     return (
       <div className="App">
         <Landing />
         <div className="stickyContainer">
-          <Feature scroll={this.state.scroll} info={this.state.featureData} active={this.state.activeFeature}/>
+          {features}
         </div>
         <footer></footer>
-        <Particles 
-        className="particleScreen"
-        params={{
-          "particles": {
-            "number": {
-              "value": 165,
-              "density": {
-                "enable": true,
-                "value_area": 800
-              }
-            },
-            "color": {
-              "value": "#ffffff"
-            },
-            "shape": {
-              "type": "circle",
-              "stroke": {
-                "width": 0,
-                "color": "#000000"
-              },
-              "polygon": {
-                "nb_sides": 5
-              },
-              "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100
-              }
-            },
-            "opacity": {
-              "value": .6,
-              "random": true,
-              "anim": {
-                "enable": true,
-                "speed": 1,
-                "opacity_min": 0,
-                "sync": false
-              }
-            },
-            "size": {
-              "value": 1,
-              "random": true,
-              "anim": {
-                "enable": false,
-                "speed": 4,
-                "size_min": 0.3,
-                "sync": false
-              }
-            },
-            "line_linked": {
-              "enable": false,
-              "distance": 150,
-              "color": "#ffffff",
-              "opacity": 0.4,
-              "width": 1
-            },
-            "move": {
-              "enable": true,
-              "speed": 1,
-              "direction": "none",
-              "random": true,
-              "straight": false,
-              "out_mode": "out",
-              "bounce": false,
-              "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 600
-              }
-            }
-          },
-          "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-              "onhover": {
-                "enable": false,
-                "mode": "bubble"
-              },
-              "onclick": {
-                "enable": true,
-                "mode": "repulse"
-              },
-              "resize": true
-            },
-            "modes": {
-              "grab": {
-                "distance": 400,
-                "line_linked": {
-                  "opacity": 1
-                }
-              },
-              "bubble": {
-                "distance": 250,
-                "size": 0,
-                "duration": 2,
-                "opacity": 0,
-                "speed": 3
-              },
-              "repulse": {
-                "distance": 178.6569867062961,
-                "duration": 0.4
-              },
-              "push": {
-                "particles_nb": 4
-              },
-              "remove": {
-                "particles_nb": 2
-              }
-            }
-          },
-          "retina_detect": true
-        }} />
+        <ParticleWrapper/>
       </div>
     );
   }
--- src/components/Feature.js
+++ src/components/Feature.js
@@ -8,10 +8,23 @@ import vid3 from '../video/RP4.mp4';
 
 const Feature = (props) => {
 
-        let headerStyles = "featureHeader fadeOut";
-        let subStyles = "featureSub fadeOut";
-        let phoneStyles = "phone fadeOut"
-        // let featureTracker = this.state.featureData[this.state.active]
+    let featureStyles, headerStyles, subStyles, phoneStyles;
+
+        if (props.active === props.info.id) {
+            featureStyles = "featureSection active"
+            headerStyles = "featureHeader fadeOut";
+            subStyles = "featureSub fadeOut";
+            phoneStyles = "phone fadeOut"
+        } else {
+            featureStyles = "featureSection"
+            headerStyles = "featureHeader fadeOut";
+            subStyles = "featureSub fadeOut";
+            phoneStyles = "phone fadeOut"
+        }
+
+        // let headerStyles = "featureHeader";
+        // let subStyles = "featureSub";
+        // let phoneStyles = "phone"
     
         if (props.scroll > 8) {
             headerStyles = "featureHeader fadeIn"
@@ -22,34 +35,34 @@ const Feature = (props) => {
         if (props.scroll > 14) {
             phoneStyles = "phone fadeIn"
         }
-        if (props.scroll > 25) {
-            headerStyles = "featureHeader refresher"
-            subStyles = "featureSub refresher"
-        }
-        if (props.scroll > 45) {
-            headerStyles = "featureHeader refresher2"
-            subStyles = "featureSub refresher2"
-        }
+        // if (props.scroll > 25) {
+        //     headerStyles = "featureHeader refresher"
+        //     subStyles = "featureSub refresher"
+        // }
+        // if (props.scroll > 45) {
+        //     headerStyles = "featureHeader refresher2"
+        //     subStyles = "featureSub refresher2"
+        // }
     
         return (
-            <div className="featureSection">
+            <div className={featureStyles}>
                 <div className={headerStyles}>
                     <div className="iconContainer">
                         <img src={report} width="100%" alt="Deeply reported icon. Search Document by Juicy Fish from the Noun Project"/>
                     </div>
                     <div className="featureHeaderText">
-                        <h2 className="featureTitle">{props.info[props.active].header}</h2>
+                        <h2 className="featureTitle">{props.info.header}</h2>
                     </div>
                 </div>
                 <div className={subStyles}>
-                    <p>{props.info[props.active].sub}</p>
+                    <p>{props.info.sub}</p>
                 </div> 
                 <div className={phoneStyles}>
                     <img src={iPhone} alt="iPhone graphic" width="60%"/>
                     <video className="demoVid" alt="introVideo" width="60%" autoPlay muted>
                         <source src={intro} type="video/mp4" />
-                        <source src={vid2} type="video/mp4" />
-                        <source src={vid3} type="video/mp4" />
+                        {/* <source src={vid2} type="video/mp4" />
+                        <source src={vid3} type="video/mp4" /> */}
                     </video>
                 </div>
             </div>
