c1648611ea1eb7f54d642b63941b31a52985963d
Tom Barnes <barnes.thomas.c@gmail.com>
2019-02-05 17:20:16 -0500

    updated styles mute button everything

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -12,19 +12,53 @@ class Delivery extends Component {
             open: false,
             active: null
         }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e){
+        if (this.state.active === null || this.state.active != e.target.id){
+            this.setState({
+                open: true,
+                active: e.target.id
+            })
+        } else if (this.state.active === e.target.id) {
+            this.setState({
+                open: false,
+                active: null
+            })
+        }
     }
 
     render() {
+
+        let optionsBox, connector;
+
+        if (this.state.open === false){
+            optionsBox = "optionsBox"
+            connector = "connector"
+        } else {
+            optionsBox = "optionsBox active"
+            connector = "connector active"
+        }
+
+        if (this.state.active === 0){
+            
+        } else if (this.state.active === 1){
+
+        } else if (this.state.active === 2){
+
+        }
+
         return(
             <div className="deliveryMaster">
                 <h2>DELIVERY OPTIONS</h2>
                 <div className="deliveryIconContainer">
-                    <div className="deliveryIcon active"><img src={cal} alt="Select calendar" width="100%"/></div>
-                    <div className="deliveryIcon"><img src={text} alt="Select text" width="100%"/></div>
-                    <div className="deliveryIcon"></div>
+                    <div className="deliveryIcon"><a className="deliveryClicker"><img id="0" onClick={this.handleClick} src={cal} alt="Select calendar" width="100%"/></a></div>
+                    <div className="deliveryIcon"><a className="deliveryClicker"><img id="1" onClick={this.handleClick} src={text} alt="Select text" width="100%"/></a></div>
+                    <div className="deliveryIcon"><a className="deliveryClicker"></a></div>
                 </div>
-                <div className="connector"></div>
-                <div className="optionsBox"> </div>
+                <div className={connector}></div>
+                <div className={optionsBox}></div>
             </div>
         )
     }
